254b61ced7d26487fb2d083b61af263308d46781
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2023-11-29 23:39:42 +0700

    dynamic web

--- Lecture3 - Django/lecture3/hello/urls.py
+++ Lecture3 - Django/lecture3/hello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='hello')
+    path('', views.index, name='hello'),
+    path('go/dung', views.dung, name='Dung'),
+    path('<str:name>', views.greet, name='greet')
 ]
--- Lecture3 - Django/lecture3/hello/views.py
+++ Lecture3 - Django/lecture3/hello/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request,'hello/index.html')
+def dung(request):
+    return HttpResponse("Hello, Dung!")
+def greet(request, name):
+    return render(request, 'hello/greet.html', {
+        'name': name.capitalize(),
+    })
